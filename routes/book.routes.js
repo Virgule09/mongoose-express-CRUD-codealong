5085a8d17cf1524efb2c1bf496d5f6a5352c1b97
“Alexandre <coma.alexandre@gmail.com>
2022-07-11 15:46:41 +0200

    added functioannlities to read and create books. Filter is not working yet.

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -6,7 +6,7 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/books", (req,res,) => {
+router.get("/books", (req,res, next) => {
 
   Book.find()
     .then( (booksFromDB) => {
@@ -18,10 +18,53 @@ router.get("/books", (req,res,) => {
     .catch( error => {
       console.log("error getting data from DB", error)
     })
-
-   
+  })
   
+router.get("/books/create", (req, res) => {
+  res.render("books/book-create");
 })
+
+router.post("/books/create", (req,res,next) => {
+  const bookDetails = {
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    rating: req.body.rating,
+  }
+
+  Book.create(bookDetails)
+    .then( () => {
+      res.redirect("/books")
+    })
+    .catch( error => {
+      console.log("error creating a book in the DB", error)
+    })
+})
+
+router.get("/books/:bookId", (req, res, next) => {
+
+  Book.findById(req.params.bookId)
+    .then( (book) => {
+    res.render("books/book-details", book);
+  })
+    .catch(error => {
+      console.log("error getting data from DB", error);
+      next(error);
+    })
+  }) 
+
+router.get("/books"), (req,res,next) => {
+  const rating = parseFloat(req.query.min-rating);
+  console.log(rating)
+  Book.find({rating: {$gte: rating}})
+  .then( (book) => {
+    res.render("books/book-details", book);
+  })
+  .catch(error => {
+    console.log("error getting data from DB", error);
+    next(error);
+  })
+}
 
 
 module.exports = router;
