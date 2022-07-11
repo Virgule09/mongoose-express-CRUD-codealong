f73eac9807fd90091a87a1bceb487c091f1ed10b
“Alexandre <coma.alexandre@gmail.com>
2022-07-11 17:24:12 +0200

    functionnality to update and delete a book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -53,12 +53,16 @@ router.get("/books/:bookId", (req, res, next) => {
     })
   }) 
 
+  // filter
 router.get("/books"), (req,res,next) => {
   const rating = parseFloat(req.query.min-rating);
   console.log(rating)
   Book.find({rating: {$gte: rating}})
-  .then( (book) => {
-    res.render("books/book-details", book);
+  .then( (booksfromDB) => {
+    const data = {
+      booksArr: booksFromDB
+    };
+    res.render("books/books-list", data);
   })
   .catch(error => {
     console.log("error getting data from DB", error);
@@ -66,5 +70,51 @@ router.get("/books"), (req,res,next) => {
   })
 }
 
+// update
+router.get("/books/:bookId/edit", (req,res,next) => {
+  
+  Book.findById(req.params.bookId)
+  .then( (bookDetails) => {
+    res.render("books/book-edit", bookDetails)
+  })
+  .catch(error => {
+    console.log("error updating a book", error);
+    next(error);
+  })
+})
+
+router.post("/books/:bookId/edit"), (req,res,next) => {
+
+  const bookId = req.params.bookId;
+  const newDetails = {
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    rating: req.body.rating,
+  };
+  Book.findByIdAndUpdate(bookId, newDetails)
+  .then( () => {
+    res.redirect("/books")
+  })
+  .catch(error => {
+    console.log("error updating a book", error);
+    next(error);
+  })
+}
+
+// Delete
+router.post("/books/:bookId/delete", (req, res) => {
+  const {bookId} = req.params;
+
+  Book.findByIdAndRemove(bookId)
+  .then( () => {
+    res.redirect("/books")
+  })
+  .catch(error => {
+    console.log("error deleting a book", error);
+    next(error);
+  })
+})
+  
 
 module.exports = router;
